d8e67b5f7217c46ac8446415711286a1b950a435
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2026-05-22 18:17:44 -0600

    Validate BL99 column thermodynamics against CICE

--- docs/make.jl
+++ docs/make.jl
@@ -59,6 +59,9 @@ pages = [
     ],
 
     "Examples" => example_pages,
+    "Validation" => [
+        "Bitz-Lipscomb CICE" => "validation/bitz_lipscomb_cice.md",
+    ],
     "Timestepping" => "timestepping.md",
 
     "Library" => [

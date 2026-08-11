ab17c3cbd97b01a330fa052d353c4146e641fd84
rafaelabel <rafael.abel@ventus-tech.de>
2026-08-12 00:11:17 +0200

    Align PR141 ocean builder with ORCA baseline

--- experiments/pr59_orca_1deg/patches/numericalearth_pr59_ecco_pr141_bl99_8layer_init_patch.jl
+++ experiments/pr59_orca_1deg/patches/numericalearth_pr59_ecco_pr141_bl99_8layer_init_patch.jl
@@ -133,9 +133,6 @@ function build_ocean(config, grid;
     κ_symmetric = OMIPSimulations.resolve_nemo_coefficient(κ_symmetric, nemo_eddy_coefficients, :symmetric_coefficient)
     κ_skew = OMIPSimulations.resolve_cesm_coefficient(κ_skew, cesm_eddy_coefficients, :skew_coefficient)
     κ_symmetric = OMIPSimulations.resolve_cesm_coefficient(κ_symmetric, cesm_eddy_coefficients, :symmetric_coefficient)
-    κ_skew = OMIPSimulations.resolve_hybrid_coefficient(κ_skew, hybrid_eddy_coefficients, :skew_coefficient)
-    κ_symmetric = OMIPSimulations.resolve_hybrid_coefficient(κ_symmetric, hybrid_eddy_coefficients, :symmetric_coefficient)
-
     if !isnothing(κ_skew) && !isnothing(κ_symmetric)
         κ_skew, κ_symmetric = OMIPSimulations.fold_safe_constant_coefficients(grid, κ_skew, κ_symmetric)
     end
